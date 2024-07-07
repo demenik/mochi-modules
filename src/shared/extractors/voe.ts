a19ff32d9e4b51e3e67194336f57ecbe6ed17b91
Dominik Bernroider <dominik.bernroider@icloud.com>
2024-07-08 00:19:37 +0200

    Fix Voe Extractor

--- src/shared/extractors/voe.ts
+++ src/shared/extractors/voe.ts
@@ -1,7 +1,11 @@
 import { ExtractorResult } from ".";
 
 export async function voe(url: string): Promise<ExtractorResult> {
-  const response = await request.get(url);
+  const res = await request.get(url);
+  const html = res.text();
+  const providerUrl = html.match(/window\.location\.href = '([^']*)'/)![1];
+
+  const response = await request.get(providerUrl);
 
   return {
     url: response.text().match(/'hls': '([^']+)'/)![1],
